4f03e42faac66553703055f74726138014586732
مصطفى عبد عثمان <85865415+mostafaothmann@users.noreply.github.com>
2025-12-17 22:20:12 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      // add your production frontend later
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
 
