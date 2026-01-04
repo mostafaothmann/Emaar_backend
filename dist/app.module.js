1d031bd0c47b2935d1dcaa5bd2272637a96dcc40
unknown <othmanmostafa373@gmail.com>
2026-01-04 22:22:25 +0300

    edititng databaseholder

--- dist/app.module.js
+++ dist/app.module.js
@@ -11,32 +11,11 @@ const common_1 = require("@nestjs/common");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
 const typeorm_1 = require("@nestjs/typeorm");
-const company_module_1 = require("./company/company.module");
-const typeofcompany_module_1 = require("./typeofcompany/typeofcompany.module");
-const p_status_module_1 = require("./p_status/p-status.module");
-const typeofproperty_module_1 = require("./typeofproperty/typeofproperty.module");
-const typeofworker_module_1 = require("./typeofworker/typeofworker.module");
-const worker_module_1 = require("./worker/worker.module");
-const customer_module_1 = require("./customer/customer.module");
-const customer_company_module_1 = require("./customer_company/customer_company.module");
-const material_module_1 = require("./material/material.module");
-const typeofmaterial_module_1 = require("./typeofmaterial/typeofmaterial.module");
-const typeofwork_module_1 = require("./typeofwork/typeofwork.module");
-const typeofownering_module_1 = require("./typeofwnering/typeofownering.module");
-const property_module_1 = require("./property/property.module");
-const videoofproperty_module_1 = require("./videoofproperty/videoofproperty.module");
-const photoofproperty_module_1 = require("./photoofproperty/photoofproperty.module");
-const worker_company_module_1 = require("./worker_company/worker_company.module");
-const company_property_offer_module_1 = require("./company_property_offer/company_property_offer.module");
-const auth_module_1 = require("./auth/auth.module");
-const customer_property_offer_module_1 = require("./customer-property-offer/customer-property-offer.module");
-const governorate_module_1 = require("./governorate/governorate.module");
-const under_governorate_module_1 = require("./under-governorate/under-governorate.module");
 const platform_express_1 = require("@nestjs/platform-express");
 const image_controller_1 = require("./image.controller");
 const cloudinary_service_1 = require("./cloudinary.service");
 require("dotenv").config();
-const DBurl = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQL_ROOT_PASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQL_DATABASE}`;
+const DBurl = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQL_PASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQL_DATABASE}?ssl-mode=REQUIRED`;
 let AppModule = class AppModule {
 };
 exports.AppModule = AppModule;
@@ -46,31 +25,10 @@ exports.AppModule = AppModule = __decorate([
             platform_express_1.MulterModule.register({ dest: './uploads' }),
             typeorm_1.TypeOrmModule.forRoot({
                 type: 'mysql',
-                url: 'mysql://avnadmin:AVNS_oicw4Pb5xMyDiVgMeer@mysql-4dd0e53-othmanmostafa373-1319.g.aivencloud.com:12466/defaultdb?ssl-mode=REQUIRED',
+                url: DBurl,
                 autoLoadEntities: true,
                 synchronize: true,
             }),
-            p_status_module_1.PStatusModule,
-            company_module_1.CompanyModule,
-            typeofcompany_module_1.TypeOfCompanyModule,
-            typeofproperty_module_1.TypeofpropertyModule,
-            typeofwork_module_1.TypeofworkModule,
-            typeofownering_module_1.TypeofOwneringModule,
-            videoofproperty_module_1.VideoofpropertyModule,
-            photoofproperty_module_1.PhotoofpropertyModule,
-            typeofworker_module_1.TypeofworkerModule,
-            worker_module_1.WorkerModule,
-            customer_module_1.CustomerModule,
-            customer_company_module_1.CustomerCompanyModule,
-            property_module_1.PropertyModule,
-            material_module_1.MaterialModule,
-            typeofmaterial_module_1.TypeofmaterialModule,
-            governorate_module_1.GovernorateModule,
-            under_governorate_module_1.UnderGovernorateModule,
-            worker_company_module_1.WorkerCompanyModule,
-            company_property_offer_module_1.CompanyPropertyOfferModule,
-            auth_module_1.AuthModule,
-            customer_property_offer_module_1.CustomerPropertyOfferModule,
         ],
         controllers: [app_controller_1.AppController, image_controller_1.ImageController],
         providers: [app_service_1.AppService, cloudinary_service_1.CloudinaryService],
