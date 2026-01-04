11a48be19e27290c7a0bfdefe3fa5486f8d32b10
unknown <othmanmostafa373@gmail.com>
2026-01-04 12:59:12 +0300

    edititng main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+
+const server = express();
+
+export default server; // 👈 MUST be top-level export
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(server),
+  );
 
-  app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      // add your production frontend later
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
-
-  const port = process.env.PORT || 3000;
-  await app.listen(port, '0.0.0.0');
-
-  console.log(`Server running on port ${port}`);
+  app.enableCors();
+  await app.init();
 }
+
 bootstrap();
